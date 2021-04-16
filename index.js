f4723734fd0822dc79333588e8dd6e2b022e32d8
F3L1P3D4RK <65518163+F3L1P3D4RK@users.noreply.github.com>
2021-04-16 14:07:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ client.on("message", msg => {
   if(msg.content === `<@${client.user.id}>` || msg.content === `<@!${client.user.id}>`) return message.channel.send(`quem me chamou?`)
 })
 
-client.login(config.token
+client.login(config.token)
