86e613b2607053bceeb65d747980643b4638638d
F3L1P3D4RK <65518163+F3L1P3D4RK@users.noreply.github.com>
2021-04-16 14:07:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,30 @@ const client = new discord.Client({
   disableEveryone : true
 })
 
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+
+["command"].forEach(h => {
+  require(`./handlers/${h}`)(client)
+})
+
 client.on("ready", () => {
   client.user.setPresence({ activity : { name : "Estou sendo feita na repl.it", type : "LISTENING" }, status : "dnd" })
   console.log(`Logado com o bot ${client.user.tag}`)
+})
+
+client.on("message", message => {
+  if(!message.guild) return;
+  if(message.author.bot) return;
+  if(!message.content.startsWith(config.prefix)) return;
+
+  let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  let cmd = args.shift().toLowerCase();
+  if(cmd === 0) return;
+  
+  let command = client.commands.get(cmd);
+  if(!command) command = client.commands.get(client.aliases.get(cmd));
+  if(command) command.run(client, message, args);
 })
 
 client.on("message", msg => {
